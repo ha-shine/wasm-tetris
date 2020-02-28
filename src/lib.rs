64e5fdefd116b4c6bdf50c4e7d3811255e863239
Htet Aung Shine <h@shine.rocks>
2020-02-28 20:34:43 +0800

    Add a few more fields and comments in the game struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod tetrimino;
 use wasm_bindgen::prelude::*;
 
 pub use tetrimino::*;
+use std::time::Duration;
 
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
@@ -21,9 +22,14 @@ pub fn main_js() -> Result<(), JsValue> {
     // This provides better error messages in debug mode.
     // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
+        console_error_panic_hook::set_once();
 
     Ok(())
+}
+
+enum State {
+    Playing,
+    Lost,
 }
 
 struct ActivePiece {
@@ -46,11 +52,33 @@ pub struct Game {
     // current playing piece and it's x, y coordinate
     // x, y coordinate is isize because the pieces can go out of bound
     active_piece: ActivePiece,
+
+    // type of tetrimino currently being held, it can be empty
+    // can_hold flag to indicate whether the player can hold the active piece
+    // when a piece is held, it can be exchanged only on when the active piece change
+    held_type: Option<PieceType>,
+    can_hold: bool,
+
+    state: State,
+
+    // elapsed time since the last update
+    elapsed: Duration,
+
+    // fall rate defines how fast a piece fall
+    // if time delta greater than this is elapsed, we move one piece down by one unit
+    fall_rate: Duration,
+
+    // indexes of lines which can be cleared/erased
+    clearable_lines: Vec<u8>,
+
+    // vector to hold pairs of x,y coordinates for current active piece's individual squares
+    // have to do this way because there is no other good way to pass a vector
+    // without incurring performance cost for serializing into js
+    active_piece_coords: Vec<usize>,
 }
 
 #[wasm_bindgen]
 impl Game {
-
     pub fn new(width: usize, height: usize) -> Game {
         let mut generator = SevenGenerator::new();
         let mut next_pieces = Vec::new();
@@ -58,19 +86,22 @@ impl Game {
         next_pieces.push(generator.next().unwrap());
         next_pieces.push(generator.next().unwrap());
 
-        let active_piece = ActivePiece {
-            piece: Tetrimino::from(generator.next().unwrap()),
-            x: 0,
-            y: 0,
-        };
+        let active_piece = Self::initialize_tetrimino(generator.next().unwrap());
 
         Game {
             width,
             height,
-            board: vec![Color::None; width*height],
+            board: vec![Color::None; width * height],
             generator,
             next_pieces,
-            active_piece
+            active_piece,
+            held_type: None,
+            can_hold: true,
+            state: State::Playing,
+            elapsed: Duration::from_micros(0),
+            fall_rate: Duration::from_millis(500), // TODO: this should update
+            clearable_lines: Vec::new(),
+            active_piece_coords: Vec::new(),
         }
     }
 
@@ -82,6 +113,35 @@ impl Game {
         self.next_pieces.as_ptr()
     }
 
+    pub fn active_piece(&self) -> *const u8 {
+        unimplemented!()
+    }
+
+    pub fn update(&mut self, elapsed: u64) {
+        let elapsed = Duration::from_micros(elapsed);
+        self.elapsed += elapsed;
+
+        if self.elapsed >= self.fall_rate {
+            self.elapsed -= self.fall_rate;
+            self.active_piece.y += 1;
+        }
+    }
+
+    // apply displacements to move the next active tetrimino into the center of board
+    fn initialize_tetrimino(ttype: PieceType) -> ActivePiece {
+        let (x, y) = match ttype {
+            PieceType::I => (3, -1),
+            _ => (3, 0)
+        };
+
+        let current_tetrimino = ActivePiece {
+            piece: Tetrimino::from(ttype),
+            x,
+            y,
+        };
+
+        current_tetrimino
+    }
 }
 
 impl Game {
@@ -100,7 +160,6 @@ struct SevenGenerator {
 }
 
 impl SevenGenerator {
-
     fn new() -> SevenGenerator {
         use PieceType::*;
 
@@ -112,7 +171,6 @@ impl SevenGenerator {
             index: 0,
         }
     }
-
 }
 
 impl Iterator for SevenGenerator {
